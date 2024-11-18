e5e6b0ba3be79d9d8424ef51072c1c66d17305fc
rhusiev <63148080+rhusiev@users.noreply.github.com>
2024-11-18 09:14:03 +0200

    rename all to pet

--- lua/pet/init.lua
+++ lua/pet/init.lua
@@ -1,11 +1,11 @@
 local M = {}
 local conf = { n_skip_above = 1, n_skip_below = 3, n_skip_right = 2, n_skip_left = 0, radius_around_cursor = 3 }
-local n_penguins = 0
+local n_pets = 0
 local do_party = false
 
-M._choose_new_spot = function(penguin_length)
+M._choose_new_spot = function(pet_length)
 	local y = math.random(vim.o.lines - conf.n_skip_above - conf.n_skip_below) + conf.n_skip_above
-	local x = math.random(vim.o.columns - penguin_length)
+	local x = math.random(vim.o.columns - pet_length)
 
 	return x, y
 end
@@ -24,15 +24,9 @@ M._choose_next_spot = function(config)
 		y = y + 1
 	end
 
-	if y < conf.n_skip_above then
-		y = vim.o.lines - conf.n_skip_below
-	elseif y > vim.o.lines - conf.n_skip_below then
-		y = conf.n_skip_above
-	end
-
 	local cursor_y = vim.api.nvim_win_get_cursor(0)[1]
 	if conf.radius_around_cursor then
-		for i = 1, conf.radius_around_cursor do
+		for i = 0, conf.radius_around_cursor do
 			if y == cursor_y - i then
 				if direction == 1 or direction == 2 then
 					y = y - 1
@@ -56,32 +50,38 @@ M._choose_next_spot = function(config)
 		x = conf.n_skip_left
 	end
 
+	if y < conf.n_skip_above then
+		y = vim.o.lines - conf.n_skip_below
+	elseif y > vim.o.lines - conf.n_skip_below then
+		y = conf.n_skip_above
+	end
+
 	config["col"] = x
 	config["row"] = y
 
 	return x, y
 end
 
-M.add_penguin = function(step_period, wait_pediod, penguin_string, repeats, attached_to_party)
-    n_penguins = n_penguins + 1
+M.add_pet = function(step_period, wait_pediod, pet_string, repeats, attached_to_party)
+    n_pets = n_pets + 1
 	if not step_period then
 		step_period = 700
 	end
 	if not wait_pediod then
 		wait_pediod = 1000
 	end
-	if not penguin_string then
-		penguin_string = "🐧"
+	if not pet_string then
+		pet_string = "🐧"
 	end
     if not repeats then
         repeats = 10
     end
-	local penguin_length = #penguin_string
+	local pet_length = #pet_string
 
 	local buf = vim.api.nvim_create_buf(false, true)
-	vim.api.nvim_buf_set_lines(buf, 0, 1, true, { penguin_string })
-	local x, y = M._choose_new_spot(penguin_length)
-	local penguin = vim.api.nvim_open_win(buf, false, {
+	vim.api.nvim_buf_set_lines(buf, 0, 1, true, { pet_string })
+	local x, y = M._choose_new_spot(pet_length)
+	local pet = vim.api.nvim_open_win(buf, false, {
 		relative = "editor",
 		style = "minimal",
 		row = y,
@@ -97,22 +97,22 @@ M.add_penguin = function(step_period, wait_pediod, penguin_string, repeats, atta
 		wait_pediod,
 		step_period,
 		vim.schedule_wrap(function()
-			local config = vim.api.nvim_win_get_config(penguin)
+			local config = vim.api.nvim_win_get_config(pet)
 			M._choose_next_spot(config)
-			vim.api.nvim_win_set_config(penguin, config)
+			vim.api.nvim_win_set_config(pet, config)
 			if i == repeats or attached_to_party and not do_party then
 				timer:close()
 				vim.api.nvim_buf_delete(buf, { force = true })
-                n_penguins = n_penguins - 1
+                n_pets = n_pets - 1
 			end
 			i = i + 1
 		end)
 	)
 end
 
-M.start_penguin_party = function(max_penguins, spawn_period, step_period, wait_pediod, penguin_string, repeats)
-    if not max_penguins then
-        max_penguins = 10
+M.start_pet_party = function(max_pets, spawn_period, step_period, wait_pediod, pet_string, repeats)
+    if not max_pets then
+        max_pets = 4
     end
     if not spawn_period then
         spawn_period = 500
@@ -124,8 +124,8 @@ M.start_penguin_party = function(max_penguins, spawn_period, step_period, wait_p
     end
     do_party = true
     spawner:start(500, spawn_period, vim.schedule_wrap(function()
-        if n_penguins < max_penguins then
-            M.add_penguin(step_period, wait_pediod, penguin_string, repeats, true)
+        if n_pets < max_pets then
+            M.add_pet(step_period, wait_pediod, pet_string, repeats, true)
         end
         if not do_party then
             spawner:close()
@@ -133,7 +133,7 @@ M.start_penguin_party = function(max_penguins, spawn_period, step_period, wait_p
     end))
 end
 
-M.stop_penguin_party = function()
+M.stop_pet_party = function()
     do_party = false
 end
 
